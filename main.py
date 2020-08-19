0b3ec1ff76c521e4dbf51d5c6b57971da1062248
Kiyoshi Yamashita <53377829+kirinboy96@users.noreply.github.com>
2020-08-20 00:12:45 +0900

    Update main.py
    わかりにくい変数名を変更しました

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from making_csv import make_csv
 
 file_name = "foodie.csv"
 
-panpee = People()
-panpee.say_hello()
+People = People()
+People.say_hello()
 
 while True:
     name = input()
     if len(name)>=1:break
 
-panpee.asking(name)
+People.asking(name)
 
 while True:
     food = input().capitalize()
@@ -19,4 +19,4 @@ while True:
 making_csv = make_csv(file_name)
 making_csv.load_data()
 making_csv.increase(food)
-panpee.thanks(name)
+People.thanks(name)
